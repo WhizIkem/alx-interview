e91811690fbfea2d199d8c49b0681cea25652342
whizikem <wisdomikem@gmail.com>
2023-08-06 22:36:50 +0100

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
+"""Solves the lock boxes puzzle """
 
 def canUnlockAll(boxes):
     """Determines if all the boxes can be opened."""
     num_boxes = len(boxes)
-    visited = [False] * num_boxes #Initialize all boxes as unvisited
-    visited[0] = True # Mark first box as visited
+    visited = [False] * num_boxes
+    visited[0] = True
 
-    stack = [0] # Initialize stack with first box
+    stack = [0]
 
     while stack:
-        current_box = stack.pop() # Take the top box from the stack
+        current_box = stack.pop()
 
-        # Iterate through the keys in the current box
         for key in boxes[current_box]:
             if key < num_boxes and not visited[key]:
-                # If the key is valid and the box has not been visited, mark it as visited and add it to the stack
                 visited[key] = True
                 stack.append(key)
 
-    return all(visited) # Return True if all boxes have been visited, False otherwise
+    return all(visited)
