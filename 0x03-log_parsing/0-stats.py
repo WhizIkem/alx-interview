05cc9996d32ff3877aeaf991ed34dadd8eb146f6
whizikem <wisdomikem@gmail.com>
2023-08-18 10:16:53 +0100

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,42 @@
 #!/usr/bin/python3
 
+"""Script that reads stdin line by line and computes metrics"""
+
 import sys
 
-def print_stats(total_file_size, status_counts):
-    print("File size: {}".format(total_file_size))
-    for code in sorted(status_counts.keys()):
-        count = status_counts[code]
-        if count > 0:
-            print("{}: {}".format(code, count))
+def printsts(dic, size):
+    """Prints the stats"""
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
-total_file_size = 0
-status_counts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0,
-                 "405": 0, "500": 0}
-line_count = 0
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+        "404": 0, "405": 0, "500": 0}
+
+count = 0
+size = 0
 
 try:
     for line in sys.stdin:
-        line = line.strip()
-        parts = line.split()
+        if count != 0 and count % 10 == 0:
+            printsts(sts, size)
 
-        if len(parts) == 10 and parts[5] == '"GET' and parts[9] == "HTTP/1.1\"":
-            try:
-                file_size = int(parts[-1])
-                status_code = parts[-2]
-                if status_code in status_counts:
-                    status_counts[status_code] += 1
-                total_file_size += file_size
-                line_count += 1
+        stlist = line.split()
+        count += 1
 
-                if line_count == 10:
-                    print_stats(total_file_size, status_counts)
-                    line_count = 0
-            except:
-                pass
+        try:
+            size += int(stlist[-1])
+        except:
+            pass
+
+        try:
+            if stlist[-2] in sts:
+                sts[stlist[-2]] += 1
+        except:
+            pass
+        printsts(sts, size)
+
 except KeyboardInterrupt:
-    print_stats(total_file_size, status_counts)
+    printsts(sts, size)
+    raise
