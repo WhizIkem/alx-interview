added0c8825463ca3f216a660ce254e2a3aac036
whizikem <wisdomikem@gmail.com>
2023-08-18 09:12:52 +0100

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,53 +2,35 @@
 
 import sys
 
-
-def print_msg(dict_sc, total_file_size):
-    """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
-    """
-
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
-
+# Dictionary to store status code counts
+status_code_counts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
 
 total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+line_count = 0
+
+def print_statistics():
+    print("Total file size: File size: {}".format(total_file_size))
+    for status_code in sorted(status_code_counts.keys()):
+        if status_code_counts[status_code] != 0:
+            print("{}: {}".format(status_code, status_code_counts[status_code]))
 
 try:
     for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
+        line = line.strip()
+        parts = line.split()
+        
+        if len(parts) == 10 and parts[5] == '"GET' and parts[9] == "HTTP/1.1\"":
+            try:
+                file_size = int(parts[-1])
+                status_code = parts[-2]
+                if status_code in status_code_counts:
+                    status_code_counts[status_code] += 1
+                total_file_size += file_size
+                line_count += 1
 
-        if len(parsed_line) > 2:
-            counter += 1
-
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])  # file size
-                code = parsed_line[1]  # status code
-
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
-
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
-
-finally:
-    print_msg(dict_sc, total_file_size)
+                if line_count % 10 == 0:
+                    print_statistics()
+            except:
+                pass
+except KeyboardInterrupt:
+    print_statistics()
