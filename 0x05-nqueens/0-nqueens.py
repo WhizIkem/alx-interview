c06cff6a7d14025d28e8d51dfcfa42a949f76f10
whizikem <wisdomikem@gmail.com>
2023-09-05 18:44:39 +0100

    N Queen

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+import sys
+
+def is_safe(board, row, col, N):
+    # Check if there is a queen in the same column
+    for i in range(row):
+        if board[i][col] == 'Q':
+            return False
+
+    # Check upper-left diagonal
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 'Q':
+            return False
+
+    # Check upper-right diagonal
+    for i, j in zip(range(row, -1, -1), range(col, N)):
+        if board[i][j] == 'Q':
+            return False
+
+    return True
+
+def print_solution(solution):
+    for pos in solution:
+        print(pos)
+    print()
+
+def solve_nqueens(N):
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [['.' for _ in range(N)] for _ in range(N)]
+    solutions = []
+
+    def backtrack(row):
+        if row == N:
+            solution = []
+            for i in range(N):
+                for j in range(N):
+                    if board[i][j] == 'Q':
+                        solution.append([i, j])
+            solutions.append(solution)
+            return
+        for col in range(N):
+            if is_safe(board, row, col, N):
+                board[row][col] = 'Q'
+                backtrack(row + 1)
+                board[row][col] = '.'
+
+    backtrack(0)
+
+    for solution in solutions:
+        print_solution(solution)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+        solve_nqueens(N)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
