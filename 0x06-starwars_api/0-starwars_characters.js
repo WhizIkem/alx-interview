60005333222701b8747f18f2740efb250e74b720
whizikem <wisdomikem@gmail.com>
2023-09-11 06:26:45 -0700

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -8,19 +8,19 @@ const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + myArgs[0];
 
 request(url, async function (error, response, body) {
-    if (!error) {
-        const json = JSON.parse(body);
-        const endpoints = json.characters;
-        for (const endpoint of endpoints) {
-            await new Promise(function (resolve, reject) {
-                request(endpoint, function (error, response, body) {
-                    if (!error) {
-                        console.log(JSON.parse(body).name);
-                        resolve();
-                    }
-                });
-            });
-        }
+  if (!error) {
+    const json = JSON.parse(body);
+    const endpoints = json.characters;
+    for (const endpoint of endpoints) {
+      await new Promise(function (resolve, reject) {
+        request(endpoint, function (error, response, body) {
+          if (!error) {
+            console.log(JSON.parse(body).name);
+            resolve();
+          }
+        });
+      });
     }
+  }
 });
 
