0fed749b5a372c4f1d5af71cc2b31a89dba8a75e
whizikem <wisdomikem@gmail.com>
2023-09-19 07:34:25 -0700

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
+"""
+Given a pile of coins of different values, determine the
+fewest number of coins needed to meet a given amount total.
+"""
 
 
 def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    dp = [total +1] * (total +1)
+    dp = [total + 1] * (total + 1)
 
     dp[0] = 0
 
-    for i in range(1, total +1):
+    for i in range(1, total + 1):
         for coin in coins:
             if coin <= i:
                 dp[i] = min(dp[i], dp[i - coin] + 1)
