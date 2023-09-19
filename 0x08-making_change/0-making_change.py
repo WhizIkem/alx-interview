8132b355fa51e0b16e2d5655827118bb2b28e2a0
whizikem <wisdomikem@gmail.com>
2023-09-19 07:43:47 -0700

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,15 +9,18 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
+    # Initialize a list to store the fewest coins
+    # Initialize with a value higher than the maximum possibel.
     dp = [total + 1] * (total + 1)
 
+    # There are 0 ways to make change for 0
     dp[0] = 0
 
     for i in range(1, total + 1):
         for coin in coins:
             if coin <= i:
                 dp[i] = min(dp[i], dp[i - coin] + 1)
-                
+
     if dp[total] == total + 1:
         return -1
     return dp[total]
