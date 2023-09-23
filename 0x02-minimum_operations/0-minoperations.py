b98663a904aa2fd911cd5e5d927b6ed866ad4a74
whizikem <wisdomikem@gmail.com>
2023-09-23 02:28:51 -0700

    Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def minOperations(n):
+    """
+    Determines the fewest number of operations needed to result
+    in exactly n H characters in the file
+    """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+
+    return operations
+
