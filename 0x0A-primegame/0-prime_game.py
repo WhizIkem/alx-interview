2bfea203ec3774fb6b02f7d02686a4ee031bc1ec
whizikem <wisdomikem@gmail.com>
2023-10-14 06:17:58 -0700

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -17,37 +17,40 @@ def isWinner(x, nums):
     """
 
 
-    def isPrime(num):
-    if num <= 1:
-      return False
-    if num <= 3:
-      return True
-    if num % 2 == 0 or num % 3 == 0:
-      return False
-    i = 5
-    while i * i <= num:
-      if num % i == 0 or num % (i + 2) == 0:
-        return False
-      i += 6
-      return True
+    def is_prime(num):
+        if num <= 1:
+            return False
+        if num <= 3:
+            return True
+        if num % 2 == 0 or num % 3 == 0:
+            return False
+        i = 5
+        while i * i <= num:
+            if num % i == 0 or num % (i + 2) == 0:
+                return False
+            i += 6
+        return True
     
     def can_win(n):
-        if n == 1:
+        # If n is even or prime, Maria wins; otherwise, Ben wins.
+        if n % 2 == 0 or is_prime(n):
             return "Ben"
-        elif is_prime(n):
-            return "Maria"
         else:
-            return "Ben"
-      
-    winner_counts = {"Maria": 0, "Ben": 0}
+            return "Maria"
 
+    maria_wins = 0
+    ben_wins = 0
+      
     for n in nums:
         winner = can_win(n)
-        winner_counts[winner] += 1
+        if winner == "Maria":
+            maria_wins += 1
+        elif winner == "Ben":
+            ben_wins += 1
 
-    if winner_counts["Maria"] > winner_counts["Ben"]:
+    if maria_wins > ben_wins:
         return "Maria"
-    elif winner_counts["Maria"] < winner_counts["Ben"]:
+    elif maria_wins < ben_wins:
         return "Ben"
     else:
         return None
