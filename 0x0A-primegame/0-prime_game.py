ff32f29ce422d139c2565597ea4b468c64c160e7
whizikem <wisdomikem@gmail.com>
2023-10-14 12:48:24 +0100

    Prime Game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+def isWinner(x, nums):
+  def isPrime(num):
+    if num <= 1:
+      return False
+    if num <= 3:
+      return True
+    if num % 2 == 0 or num % 3 == 0:
+      return False
+    i = 5
+    while i * i <= num:
+      if num % i == 0 or num % (i + 2) == 0:
+        return False
+      i += 6
+      return True
+    
+    def can_win(n):
+      if n % 2 == 1:
+        return "Maria"
+      else:
+        return "Ben"
+      
+      winner_counts = {"Maria": 0, "Ben": 0}
+
+      for n in nums:
+        winner = can_win(n)
+        winner_counts[winner] += 1
+
+        if winner_counts["Maria"] > winner_counts["Ben"]:
+          return "Maria"
+        elif winner_counts["Ben"] > winner_counts["Maria"]:
+          return "Ben"
+        else:
+          return None
